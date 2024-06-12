70beef15e3b0cd3eb2b651131050cba7353937b4
Wes Oldt <wes.oldt@gmail.com>
2024-06-12 09:47:41 -0700

    add word-list and util method

---
+++ src/utils/word-hints.ts
@@ -0,0 +1,31 @@
+const wordListFilter = (
+  excludedLetters: string,
+  letterPositions: [{ letter: string; position: number }],
+  includedLetters: [{ letter: string; notPositions: [number] }],
+  word: string
+): boolean => {
+  const letters = word.split('');
+  const excludedLettersArr = excludedLetters.split('');
+  return (
+    excludedLettersArr.every(
+      (excludedLetter) => !letters.includes(excludedLetter)
+    ) &&
+    letterPositions.every(
+      ({ letter, position }) => letters[position] === letter
+    ) &&
+    includedLetters.every(
+      ({ letter, notPositions }) =>
+        letters.includes(letter) &&
+        !notPositions.includes(letters.indexOf(letter))
+    )
+  );
+};
+
+export const wordListFilterCurry = (
+  excludedLetters: string,
+  letterPositions: [{ letter: string; position: number }],
+  includedLetters: [{ letter: string; notPositions: [number] }]
+): ((word: string) => boolean) => {
+  return (word: string): boolean =>
+    wordListFilter(excludedLetters, letterPositions, includedLetters, word);
+};
