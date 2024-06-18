6ba3d9bb71d16483c949138f4dee17e21a7fd617
Wes Oldt <wes.oldt@gmail.com>
2024-06-17 20:58:19 -0700

    trying to fix tailwind dup styles; fix Button

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,21 @@ import { Button } from '@/components/ui/';
 
 function App() {
   return (
-    <div
-      className="border-pig-purple-800 container mx-auto flex flex-col justify-center border-4
-        px-4"
-    >
+    <div className="container mx-auto flex flex-col justify-center px-4">
       <img src={oinkleLogo} alt="Oinkdle logo" />
-      <h2 className="text-pig-pink text-center">
-        A word guessing game for pigs
-      </h2>
+      <h2 className="text-center">A word guessing game for pigs</h2>
       <div className="flex justify-center">
         <button
-          className="ring-offset-background focus-visible:ring-ring border-pig-purple-800
-            bg-pig-purple-300 hover:bg-pig-purple-400 m-4 rounded-2xl border px-4 py-2
-            transition-all focus-visible:outline-none focus-visible:ring-2
-            focus-visible:ring-offset-2"
+          className="focus:ring-pig-purple-300 border-pig-purple-800 bg-pig-purple-300
+            hover:bg-pig-purple-400 rounded-2xl border px-4 py-2 transition-all
+            focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
         >
           Play
         </button>
-        <Button>Primary</Button>
-        <Button variant="secondary" size={'lg'}>
-          Secondary
-        </Button>
-        <Button variant="oink" size={'lg'}>
-          Oink
-        </Button>
+        <Button>Default (Primary)</Button>
+        <Button variant="inversePrimary">Inverse Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="oink">Oink</Button>
       </div>
       <BreakPointIdentifier />
     </div>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,17 +4,25 @@ import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
-  `focus-visible:ring-pig-pink-950 dark:ring-offset-pig-pink-950
-  dark:focus-visible:ring-pig-pink-300 inline-flex items-center justify-center
-  whitespace-nowrap rounded-md text-sm font-medium ring-offset-white
-  transition-all focus-visible:outline-none focus-visible:ring-2
-  focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`,
+  `dark:boarder-0 m-1 inline-flex items-center justify-center whitespace-nowrap
+  rounded-2xl border-0 text-sm font-semibold transition-all focus:outline-none
+  focus:ring-0 focus-visible:outline-none focus-visible:ring-2
+  disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none
+  dark:focus:ring-offset-zinc-800 dark:focus-visible:outline-none`,
   {
     variants: {
       variant: {
-        default: [
-          `bg-pig-pink-900 text-pig-pink-50 hover:bg-pig-pink-900/90 dark:bg-pig-pink-50
-          dark:text-pig-pink-900 dark:hover:bg-pig-pink-50/90 active:bg-pig-pink-950`,
+        primary: [
+          `focus:ring-pig-pink-400/85 bg-pig-pink-300 hover:bg-pig-pink-400/85
+          active:bg-pig-pink-400 text-pig-pink-950 dark:bg-pig-pink-900
+          dark:text-pig-pink-100 dark:hover:bg-pig-pink-900/85 dark:active:bg-pig-pink-900
+          dark:focus:ring-pig-pink-900/85`,
+        ],
+        inversePrimary: [
+          `dark:focus:ring-pig-pink-400/85 dark:bg-pig-pink-300
+          dark:hover:bg-pig-pink-400/85 dark:active:bg-pig-pink-400 dark:text-pig-pink-950
+          bg-pig-pink-900 text-pig-pink-50 hover:bg-pig-pink-900/85 active:bg-pig-pink-900
+          focus:ring-pig-pink-900/85`,
         ],
         destructive: [
           `bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50
@@ -28,28 +36,24 @@ const buttonVariants = cva(
         secondary: [
           `bg-pig-purple-900 text-pig-purple-50 hover:bg-pig-purple-900/90
           dark:bg-pig-purple-50 dark:text-pig-purple-900 dark:hover:bg-pig-purple-50/90
-          active:bg-pig-purple-950`,
+          active:bg-pig-purple-950 border-0`,
         ],
         ghost: [
           `hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800
           dark:hover:text-slate-50`,
         ],
         link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
-        oink: `ring-offset-background border-pig-purple-800 bg-pig-purple-300
-        hover:bg-pig-purple-400 focus:ring-pig-pink-200 active:bg-pig-purple-500
-        dark:bg-pig-pink-100 m-4 rounded-2xl border px-4 py-2 transition-all
-        focus:outline-none focus:ring focus:ring-offset-2 focus-visible:outline-none
-        focus-visible:ring-2 focus-visible:ring-offset-2 disabled:bg-gray-300`,
+        oink: '',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 px-8',
+        default: 'h-11 px-4 py-2',
+        sm: 'h-9 px-3',
+        lg: 'h-14 px-8',
         icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default',
     },
   },
