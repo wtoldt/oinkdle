ded5e309bed270942dabdf261c4844cf25ee2191
Wes Oldt <wes.oldt@gmail.com>
2024-07-03 22:06:51 -0700

    refactor key into its own component

---
+++ src/components/game/Key.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { cn } from '@/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+//perfect px is px-[0.85rem] sm:px-6
+//widths measured on screen: 37.09px sm:59px
+const keyVariants = cva(
+  `btn flex h-14 flex-1 items-center justify-center rounded-sm font-mono text-lg
+  font-semibold sm:h-12 sm:flex-none sm:rounded-md sm:px-6 sm:text-xl sm:font-bold`,
+  {
+    variants: {
+      evaluation: {
+        correct: 'evaluation-correct',
+        present: 'evaluation-present',
+        absent: 'evaluation-absent',
+        unevaluated: 'evaluation-unevaluated',
+      },
+    },
+    defaultVariants: {
+      evaluation: 'unevaluated',
+    },
+  },
+);
+type KeyProps = React.ComponentPropsWithoutRef<'button'> &
+  VariantProps<typeof keyVariants>;
+const Key = ({ children, className, evaluation, onClick }: KeyProps) => {
+  return (
+    <button
+      className={cn(keyVariants({ evaluation }), className)}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export { Key };
--- src/components/game/Keyboard.tsx
+++ src/components/game/Keyboard.tsx
@@ -1,44 +1,10 @@
-import * as React from 'react';
-import { cn } from '@/utils';
 import { type Guess, type Evaluation } from '@/domain';
 import { CornerDownLeft, Delete } from 'lucide-react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { Key } from '@/components';
 
 const topRow = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
 const middleRow = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
 const bottomRow = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
-
-//perfect px is px-[0.85rem] sm:px-6
-//widths measured on screen: 37.09px sm:59px
-const keyVariants = cva(
-  `btn flex h-14 flex-1 items-center justify-center rounded-sm font-mono text-lg
-  font-semibold sm:h-12 sm:flex-none sm:rounded-md sm:px-6 sm:text-xl sm:font-bold`,
-  {
-    variants: {
-      evaluation: {
-        correct: 'evaluation-correct',
-        present: 'evaluation-present',
-        absent: 'evaluation-absent',
-        unevaluated: 'evaluation-unevaluated',
-      },
-    },
-    defaultVariants: {
-      evaluation: 'unevaluated',
-    },
-  },
-);
-type KeyProps = React.ComponentPropsWithoutRef<'button'> &
-  VariantProps<typeof keyVariants>;
-const Key = ({ children, className, evaluation, onClick }: KeyProps) => {
-  return (
-    <button
-      className={cn(keyVariants({ evaluation }), className)}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
 
 type KeyboardProps = {
   addLetter: (letter: string) => void;
