cf1b6a65782037ee4d2919c5e5089c241b1261ff
Wes Oldt <wes.oldt@gmail.com>
2024-07-04 08:44:44 -0700

    bugfix: check react devtools exists before disabling it

--- src/react-devtools.js
+++ src/react-devtools.js
@@ -1,4 +1,6 @@
 //prevent react devTools from being loaded in production
 if (import.meta.env.PROD) {
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () {};
+  if (window && window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () {};
+  }
 }
